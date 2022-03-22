9f47f25abcad52475d379d2c3e1f3de72390f209
Tavo <gustavo.pinto@correounivalle.edu.co>
2022-03-22 15:17:58 -0500

    added controllers to other folder

--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -1,25 +1,10 @@
 import { Router, Request, Response } from "express"
-import { getConnectionUrl, getGoogleTokens, getUserDetails } from "./../lib/google";
+import authController from "./../controllers/auth_controller";
 
 const router = Router();
 
-router.get("/google", (req: Request, res: Response) => {
-  res.redirect(getConnectionUrl());
-})
+router.get("/google", authController.googleLogin)
+router.get("/google/callback", authController.googleCallback)
 
-router.get("/google/callback", async (req: Request, res: Response) => {
-  const code = req.query.code as string;
-  try {
-    const tokens = await getGoogleTokens(code)
-    const userInfo = await getUserDetails(tokens)
-    return res.json({
-      userInfo: userInfo.email
-    })
-  }catch(e) {
-    return res.json({
-      message: "An error ocurred"
-    })
-  }
-  
-})
+
 export default router;
