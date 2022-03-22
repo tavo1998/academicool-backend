2f7bf520646f622b8e0528627f38ec664e19565f
Tavo <gustavo.pinto@correounivalle.edu.co>
2022-03-21 22:06:27 -0500

    created google auth route

--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -1,10 +1,25 @@
 import { Router, Request, Response } from "express"
+import { getConnectionUrl, getGoogleTokens, getUserDetails } from "./../lib/google";
 
 const router = Router();
 
 router.get("/google", (req: Request, res: Response) => {
-  console.log("You are going to sign in with google")
-  res.json({"message": "Login with Google..."})
+  res.redirect(getConnectionUrl());
 })
 
+router.get("/google/callback", async (req: Request, res: Response) => {
+  const code = req.query.code as string;
+  try {
+    const tokens = await getGoogleTokens(code)
+    const userInfo = await getUserDetails(tokens)
+    return res.json({
+      userInfo: userInfo.email
+    })
+  }catch(e) {
+    return res.json({
+      message: "An error ocurred"
+    })
+  }
+  
+})
 export default router;
