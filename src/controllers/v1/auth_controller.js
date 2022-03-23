8ce4c4f505e40292dcf6514ccb3899f5dfeb4d0f
Tavo <gustavo.pinto@correounivalle.edu.co>
2022-03-23 14:42:12 -0500

    separate check user auth

--- src/controllers/v1/auth_controller.js
+++ src/controllers/v1/auth_controller.js
@@ -1,21 +1,14 @@
-const { getConnectionUrl, getGoogleTokens, getUserDetails } = require("../../lib/google");
+const { getConnectionUrl } = require("../../lib/google");
 const { createJWT } = require("../../lib/jwt");
 const { createOptionsCookie } = require("../../lib/cookies");
-const { checkUserRegister } = require("../../services/user_service");
  
 function googleLogin(req, res) {
   return res.redirect(getConnectionUrl());
 }
 
 async function googleCallback(req, res) {
-  const code = req.query.code;
   try {
-    const tokens = await getGoogleTokens(code)
-    const userInfo = await getUserDetails(tokens)
-    if(!checkUserRegister(userInfo.email)){
-      return res.redirect("/")
-    }
-    const jwt = createJWT({ user_id: 5 });
+    const jwt = createJWT({ userId: req.userId });
     res.cookie("user_auth_token", jwt, createOptionsCookie(1));
     return res.redirect("/")
   }catch(e) {
