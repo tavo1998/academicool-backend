cd9e5e8f4945958a02a2f0893e982e810e98d908
Tavo <gustavo.pinto@correounivalle.edu.co>
2022-03-23 14:43:56 -0500

    added middleware to google callback

--- src/routers/v1/auth.js
+++ src/routers/v1/auth.js
@@ -1,11 +1,11 @@
-const { Router, Request, Response } =  require("express");
+const { Router } =  require("express");
 const authController = require("../../controllers/v1/auth_controller");
 const checkUserExist = require("../../middlewares/check_user_exist");
 
 const router = Router();
 
 router.get("/google", authController.googleLogin)
-router.get("/google/callback", authController.googleCallback)
+router.get("/google/callback",checkUserExist ,authController.googleCallback)
 
 
 module.exports = router;
