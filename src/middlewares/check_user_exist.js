54f0b16a1f445f17225e788fd7f20243e335beed
Tavo <gustavo.pinto@correounivalle.edu.co>
2022-03-23 14:42:46 -0500

    created middleware for check user login

--- src/middlewares/check_user_exist.js
+++ src/middlewares/check_user_exist.js
@@ -3,14 +3,17 @@ const { checkUserRegister } = require("../services/user_service");
 
 async function checkUserExist(req, res, next) {
   const code = req.query.code;
+  const errorMessage = "Ocurrió un error al iniciar sesión, comunícate con soporte"
 
   try {
     const tokens = await getGoogleTokens(code)
     const userInfo = await getUserDetails(tokens)
-    if(!checkUserRegister(userInfo.email)){
-      return res.redirect("/login")
+    const user = await checkUserRegister(userInfo.email)
+    if(!user){
+      return res.redirect(`/login?error=${errorMessage}`)
     }
-    //req.userInfo = userInfo;
+    req.userInfo = userInfo;
+    req.userId = user.id
     next()
   }catch(e) {
     console.log(e)
