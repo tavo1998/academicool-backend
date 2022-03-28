dfa17e1d70852771b2197847983572983a587a80
tavo1998 <gustavo.pinto@correounivalle.edu.co>
2022-03-28 14:26:11 -0500

    added admin middleware to protected routes

--- src/routers/v1/institution.js
+++ src/routers/v1/institution.js
@@ -1,12 +1,22 @@
 const { Router } = require('express')
 const { body } = require('express-validator')
+const checkUserCookie = require('./../../middlewares/check_user_cookie')
+const checkIsAdmin = require('./../../middlewares/check_is_admin')
 const institutionController = require('../../controllers/v1/institution_controller')
 
 const router = Router()
 
-router.get('/', institutionController.getInstitutionsController)
+router.get(
+  '/',
+  checkUserCookie,
+  checkIsAdmin,
+  institutionController.getInstitutionsController
+)
+
 router.post(
   '/',
+  checkUserCookie,
+  checkIsAdmin,
   body('name').isLength({ max: 100 }).notEmpty(),
   body('mision').isLength({ max: 280 }).notEmpty().optional(),
   body('vision').isLength({ max: 280 }).notEmpty().optional(),
@@ -14,12 +24,19 @@ router.post(
   institutionController.createInstitutionController)
 
 router.put('/:id',
+  checkUserCookie,
+  checkIsAdmin,
   body('name').isLength({ max: 100 }).notEmpty().optional(),
   body('mision').isLength({ max: 280 }).notEmpty().optional(),
   body('vision').isLength({ max: 280 }).notEmpty().optional(),
   body('address').isLength({ max: 100 }).notEmpty().optional(),
   institutionController.updateInstitutionController)
 
-router.delete('/:id', institutionController.deleteInstitutionController)
+router.delete(
+  '/:id',
+  checkUserCookie,
+  checkIsAdmin,
+  institutionController.deleteInstitutionController
+)
 
 module.exports = router
