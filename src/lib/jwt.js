802f9bea5df000726fcb872891ec8e232e994dad
tavo1998 <gustavo.pinto@correounivalle.edu.co>
2022-03-28 14:25:03 -0500

    created decode json web  token fuction

--- src/lib/jwt.js
+++ src/lib/jwt.js
@@ -4,6 +4,16 @@ function createJWT (payload) {
   return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: 5 * 60 })
 }
 
+function decodeToken (token) {
+  try {
+    const decodedPayload = jwt.verify(token, process.env.JWT_SECRET)
+    return decodedPayload
+  } catch (e) {
+    return false
+  }
+}
+
 module.exports = {
-  createJWT
+  createJWT,
+  decodeToken
 }
