1b2140103ebb06ea372f907c2e9e4bfa2786f6ff
tavo1998 <gustavo.pinto@correounivalle.edu.co>
2022-04-22 15:41:53 -0500

    created fuction to get  assigment scores for each subject, group by assignment type

--- src/services/student_service.js
+++ src/services/student_service.js
@@ -1,3 +1,4 @@
+const { ASSIGNMENT_TYPE_LIST } = require('../lib/assignment')
 const prisma = require('./../config/database')
 
 const getStudentById = async (id) => {
@@ -35,7 +36,37 @@ const getStudentAssistance = async (studentId, subjectId, date) => {
   }
 }
 
+const getSubjectScoreAverage = async (studentId, subjectId) => {
+  const averageValues = {}
+
+  try {
+    for (const assignmentType of ASSIGNMENT_TYPE_LIST) {
+      const aggregation = await prisma.score.aggregate({
+        where: {
+          assigment: {
+            is_active: true,
+            assignment_type: assignmentType,
+            subject_id: subjectId
+          },
+          student: {
+            id: studentId
+          }
+        },
+        _avg: {
+          score: true
+        }
+      })
+      averageValues[assignmentType] = aggregation._avg.score
+    }
+    return averageValues
+  } catch (e) {
+    console.log(e)
+    throw e
+  }
+}
+
 module.exports = {
   getStudentById,
-  getStudentAssistance
+  getStudentAssistance,
+  getSubjectScoreAverage
 }
